2918ef276583d0a040b2f7ea51dde156e3ac4498
Atik2788 <ashanto653.2@gmail.com>
2024-08-18 08:38:58 +0600

    after doing model2

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,6 +6,7 @@ import { yellowImg } from "../utils";
 import * as THREE from 'three';
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
+import { models, sizes } from "../constants";
 
 
 const Model = () => {
@@ -41,24 +42,24 @@ const Model = () => {
 
                 <div className="flex flex-col items-center mt-5">
                     <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-                        <ModelView 
-                        index={1}
-                        groupRef={small}
-                        gsapType='view1'
-                        controlRef={cameraControlSmall}
-                        setRotationState={setSmallRotation}
-                        item={model}
-                        size={size}
+                        <ModelView
+                            index={1}
+                            groupRef={small}
+                            gsapType='view1'
+                            controlRef={cameraControlSmall}
+                            setRotationState={setSmallRotation}
+                            item={model}
+                            size={size}
                         />
 
-                        <ModelView 
-                        index={2}
-                        groupRef={large}
-                        gsapType='view2'
-                        controlRef={cameraControlLarge}
-                        setRotationState={setLargeRotation}
-                        item={model}
-                        size={size}
+                        <ModelView
+                            index={2}
+                            groupRef={large}
+                            gsapType='view2'
+                            controlRef={cameraControlLarge}
+                            setRotationState={setLargeRotation}
+                            item={model}
+                            size={size}
                         />
 
                         <Canvas
@@ -67,13 +68,13 @@ const Model = () => {
                                 position: 'fixed',
                                 top: 0,
                                 bottom: 0,
-                                left: 0, 
+                                left: 0,
                                 right: 0,
                                 overflow: 'hidden'
                             }}
                             eventSource={document.getElementById('root')}
                         >
-                            <View.Port/>
+                            <View.Port />
                         </Canvas>
                     </div>
 
@@ -82,12 +83,25 @@ const Model = () => {
                             {model.title}
                         </p>
 
-                        <div className="flex-centr">
+                        <div className="flex-center">
                             <ul className="color-container">
-                                {model.map((item, i) => (
-                                    <li key={{i}} className="w-6 h-6 rounded-full mx-2"></li>
+                                {models.map((item, i) => (
+                                    <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{ backgroundColor: item.color[0] }}
+                                        onClick={() => setModel(item)} />
                                 ))}
                             </ul>
+
+                            <button className="size-btn-container">
+                                {sizes.map(({ label, value }) => (
+                                    <span key={label} className="size-btn"
+                                        style={{
+                                            backgroundColor: size === value ? 'white' : 'transparent',
+                                            color: size === value ? 'black' : 'white'
+                                        }} onClick={() => setSize(value)} >
+                                        {label}
+                                    </span>
+                                ))}
+                            </button>
                         </div>
                     </div>
                 </div>
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,9 +1,26 @@
+import { PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Lights";
+import { Suspense } from "react";
+import IPhone from './IPhone'
 
-const ModelView = () => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, item, size }) => {
     return (
-        <div>
-            ModalView
-        </div>
+        <View
+        index={index}
+        id={gsapType}
+        className={`border-2 border-red-500 w-full h-full ${index ===2} ? 'right-[-100%]' : '' `}
+        >
+            {/* Ambient Light */}
+            <ambientLight intesnity={0.3}/>
+
+            <PerspectiveCamera makeDefault position={[0, 0, 4]}/>
+
+            <Lights/>
+
+            <Suspense fallback={<div>Loading</div>}>
+                <IPhone/>
+            </Suspense>
+        </View>
     );
 };
 
